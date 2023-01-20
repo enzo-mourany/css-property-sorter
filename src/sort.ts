c8975731f5aba4e6a85c53ffa2c4a59b63f29643
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-20 21:10:20 +0100

    feat: scss indent converved

--- src/sort.ts
+++ src/sort.ts
@@ -2,9 +2,9 @@ import * as vscode from 'vscode';
 import { ORDERED_PROPERTIES } from './orderedProperties';
 
 /**
- * Sorts the CSS properties within the active text editor.
- * @returns void
- */
+* Sorts the CSS properties within the active text editor.
+* @returns void
+*/
 export const sortProperties = () => {
   // Get the active text editor
   let editor = vscode.window.activeTextEditor as vscode.TextEditor;
@@ -16,11 +16,11 @@ export const sortProperties = () => {
   
   // Initialize an empty array to store the sorted lines
   let sortedLines: string[] = [];
-
+  
   let i: number = 0;
   while (i < lines.length) {
     let line: string = lines[i];
-    let indentation: string = '';
+    //let indentation: string = '';
     
     // Check if the line is a CSS selector
     if (line.trim().endsWith('{')) {
@@ -28,35 +28,49 @@ export const sortProperties = () => {
       let properties: string[] = [];
       let nextLine: string = lines[i + 1];
 
+      // Use regular expression to extract current indentation for the selector
+      let indentationMatch = line.match(/^\s+/);
+      let indentation = indentationMatch ? indentationMatch[0] : '';
+      
       // get the indentation in vscode settings (CSS file only)
       let workspaceIndentation = vscode.workspace.getConfiguration('editor').get('tabSize') as number;
       for (let j: number = 0; j < workspaceIndentation; j++) {
         indentation += ' ';
       }
       
-      while (!nextLine.trim().endsWith('}')) {
+      while (!nextLine.trim().endsWith('}') && !nextLine.trim().endsWith('{')) {
         // Check if the next line is a CSS property
         if (nextLine.trim().includes(':')) {
+          let indentationMatch = nextLine.match(/^\s+/);
+          indentation = indentationMatch ? indentationMatch[0] : '';
           properties.push(nextLine.trim());
         }
         i++;
         nextLine = lines[i + 1];
       }
-
+      
       // Sort the properties according to the ORDERED_PROPERTIES constant
       properties.sort((a, b) => {
         let aProp: string = a.split(':')[0].trim();
         let bProp: string = b.split(':')[0].trim();
         return ORDERED_PROPERTIES.indexOf(aProp) - ORDERED_PROPERTIES.indexOf(bProp);
       });
-
+      
       // Add the selector and sorted properties to the sortedLines array
-      sortedLines.push(selector);
+      if (document.languageId === 'css') {
+        sortedLines.push(selector);
+      } else if (document.languageId === 'scss') {
+        sortedLines.push(indentation + selector);
+      }
+
+      // Add the properties to the sortedLines array
       for (let prop of properties) {
         sortedLines.push(indentation + prop);
       }
-      if(lines[i + 1] && lines[i + 1].trim() !== '}'){
-        sortedLines.push(indentation + '}');
+      if(lines[i + 1] && lines[i + 1].trim() !== '}') {
+        if (document.languageId === 'css') {
+          sortedLines.push('}');
+        }
       }
       i++;
     } else {
