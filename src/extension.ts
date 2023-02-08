d7057f5a81adbd59c261241925b7b3474404e234
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-08 10:46:42 +0100

    feat: add errors management

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,9 @@ import { sortProperties } from './sort';
 let cssSortButton: vscode.StatusBarItem;
 
 /**
- * This method is called when your extension is activated.
- * @param context The extension context provided by VS Code.
- */
+* This method is called when your extension is activated.
+* @param context The extension context provided by VS Code.
+*/
 export function activate(context: vscode.ExtensionContext) {
 	cssSortButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 	cssSortButton.command = 'css-property-sorter.sortProperties' as string | vscode.Command;
@@ -16,12 +16,16 @@ export function activate(context: vscode.ExtensionContext) {
 	cssSortButton.show();
 	
 	let sortCommand: vscode.Disposable = vscode.commands.registerCommand('css-property-sorter.sortProperties', () => {
+		if (!vscode.window.activeTextEditor) {
+			vscode.window.showErrorMessage('Please open a file to sort');
+			return;
+		}
+		if (vscode.window.activeTextEditor?.document.languageId !== 'css' && vscode.window.activeTextEditor?.document.languageId !== 'scss') {
+			vscode.window.showErrorMessage('Please open a CSS or SCSS file to sort');
+			return;
+		}
 		vscode.window.showInformationMessage('Do you want to sort your CSS properties?', 'Cancel', 'Run').then(val => {
 			if (val === 'Run') {
-				if (vscode.window.activeTextEditor?.document.languageId !== 'css' && vscode.window.activeTextEditor?.document.languageId !== 'scss') {
-					vscode.window.showErrorMessage('Please open a CSS or SCSS file to sort');
-					return;
-				}
 				sortProperties();
 			}
 		});
